893feacbe8fcb7e9c6e4bffe36162e778f50491b
Monika <monika.silvia.s@hotmail.ca>
2021-09-09 14:24:14 -0700

    Finished settings up Models
    Created models for the databased for the store. This is the end of the django tutorial. There may be more later.

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -7,8 +7,15 @@ from django.http import HttpResponse
 # action
 
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+
 def say_hello(request):
     # pull data from a db
     # transform
     # send email
-    return render(request, 'hello.html', {'name': 'Monika'})
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Monika', 'number': x})
--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'debug_toolbar',
+    'store',
+    'tags',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,6 +69,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
 ]
 
 WSGI_APPLICATION = 'storefront.wsgi.application'
